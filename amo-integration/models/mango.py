80ac756133298767935635700c06b055afcc257a
vitaly <lildid32@mail.ru>
2025-04-30 20:59:00 +0500

    Deleted old version

--- amo-integration/models/mango.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-from pydantic import BaseModel
-
-@dataclass
-class MangoRecord:
-    record_id: str
-    temporary_url: str
-
--- amo-integration/models/pbx.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class CallRecord:
-    uuid: str
-    url: str | None
